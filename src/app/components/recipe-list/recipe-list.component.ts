5f6bb80fae2e7e691c824ae8dc5bbdc3426d599d
Courtney La Prince <claprince@courtneys-imac.home>
2018-04-17 15:44:54 -0400

    Updated code semi working app

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -12,21 +12,29 @@ export class RecipeListComponent {
 
   recipes: Recipe[];
 
+  recipe_in_progress: Recipe;
+
   constructor() {
+    this.recipe_in_progress = Recipe.createBlank();
     this.recipes = [
       new Recipe('Mac and Cheese',
         'This a one of the best Mac and Cheese recipes! Other than the one that my grandmother made. Now that is the best!',
-        null, null, null),
+        5, 45, null, null, null),
       new Recipe('Fried Chicken',
-        'This is a southern recipe that is a fan favorite of my family. I personally love me some fried chicken.',
-        null, null, null),
+        'This is a southern recipe that is a fan favorite of my family. I personally love me some fried chicken. Especially this one',
+        4, 20, null, null, null),
       new Recipe('Banana Pudding',
-        'I love love love love me some banana pudding. It\'s like the best comfort food',
-        null, null, null),
+        'I love love love love me some banana pudding. It\'s like the best comfort food, and it\'s always a hit at the family cookouts. ',
+        3, 30, null, null, null),
       new Recipe('Southern Cabbage',
         'I love love love love loooovvve cabbage. Especially with rice. I really love vegetables but cabbage is the winner.',
-        null, null, null)
+        4, 10, null, null, null)
     ];
   }
 
+  public addRecipeClicked() {
+    console.log(JSON.stringify(this.recipe_in_progress, null, 2));
+    this.recipes.unshift(this.recipe_in_progress);
+    this.recipe_in_progress = Recipe.createBlank();
+  }
 }
--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -11,15 +11,23 @@ export interface Instruction {
 export class Recipe {
     public title: string;
     public description: string;
+    public feeds_this_many: number;
+    public preparation_time: number;
     public ingredients: Ingredient[];
     public instructions: Instruction[];
     public cover_photo: string;
 
-    constructor(t: string, d: string, ingr: Ingredient[], instr: Instruction[], cp: string) {
+    constructor(t: string, d: string, feeds: number, prep: number, ingr: Ingredient[], instr: Instruction[], cp: string) {
         this.title = t;
         this.description = d;
+        this.feeds_this_many = feeds;
+        this.preparation_time = prep;
         this.ingredients = ingr;
         this.instructions = instr;
         this.cover_photo = cp;
+    }
+
+    public static createBlank() {
+        return new Recipe('', '', 1, 1, null, null, null);
     }
 }
